47a649db82683f75c414a713cfa8a16312aa16c7
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-02-09 22:03:43 +0700

    add bookingtransaction models

--- app/Models/BookingTransaction.php
+++ app/Models/BookingTransaction.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookingTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'booking_transactions';
+
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'booking_trx',
+        'total_amount',
+        'duration',
+        'started_at',
+        'ended_at',
+        'is_paid',
+        'house_id',
+    ];
+
+    protected $casts = [
+        'is_paid' => 'boolean',
+        'started_at' => 'date',
+        'ended_at' => 'date',
+    ];
+
+    public function house()
+    {
+        return $this->belongsTo(House::class);
+    }
 }
