bbc9355abe2ad253ef52b589f9a7212e22dad436
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-03-02 21:51:05 +0700

    update apikey for dashboard

--- app/Models/ApiKey.php
+++ app/Models/ApiKey.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ApiKey extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'api_keys'; // Nama tabel yang digunakan
+
+    protected $fillable = [
+        'name',
+        'key',
+    ];
+
+    protected $dates = ['deleted_at']; // Untuk soft delete
 }
