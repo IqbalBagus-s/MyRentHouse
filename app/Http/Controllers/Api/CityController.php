7d0f6635e87a2f9fb01957ec73d434c92ef6b10e
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-03-08 22:47:04 +0700

    update city and house controller for api integration

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityResource as ApiCityResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index() {
+        $cities = City::withCount('houses')->get();
+        return ApiCityResource::collection($cities); // Menggunakan collection karena akan menampilkan semua data
+    }
+
+    public function show(City $city) { // Menggunakan model bending dari laravel
+        $city->load(['houses.city', 'houses.photos']);
+        $city->load('houses');
+        return new ApiCityResource($city); // Menggunakan new karena hanya satu data yang akan ditampilkan
+    }
 }
--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\HouseResource as ApiHouseResource;
+use App\Models\House;
 use Illuminate\Http\Request;
 
 class HouseController extends Controller
 {
-    //
+    public function index() {
+        $house = House::withCount('houses')->get();
+        return ApiHouseResource::collection($house); // Menggunakan collection karena akan menampilkan semua data namun dibatasi 6
+    }
+
+    public function show(House $house) { // Menggunakan model bending dari laravel
+        $house->load(['city', 'photos', 'features']);
+        return new ApiHouseResource($house); // Menggunakan new karena hanya satu data yang akan ditampilkan
+    }
 }
